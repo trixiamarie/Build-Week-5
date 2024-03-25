daf3060b58c18c2609e8dd6da19b472fd8461f47
MonicaMisciagna <monicagofu@gmail.com>
2024-03-25 14:34:30 +0100

    grafica base genres admin

--- librarium-app/resources/views/creagenereadmin.blade.php
+++ librarium-app/resources/views/creagenereadmin.blade.php
@@ -1,39 +1,61 @@
+
+<style>
+   .glass-effect {
+   background-color: rgba(33, 107, 90, 0.2);
+   backdrop-filter: blur(10px);
+   border-radius: 1dvh;
+   border: 1px solid rgba(33, 107, 90, 0.3);
+   padding: 5dvh !important;
+   z-index: 1000;
+}
+  .btn-custom {
+      margin-top: 20px !important;
+      background-color: white !important;
+      color: #44b4b0 !important;
+      transition: background-color 1s ease !important;
+      transition: color 1s ease !important;
+      font-family: 'Silka', sans-serif !important;
+  }
+
+
+
+
+  .btn-custom:hover {
+      color: white !important;
+      background-color: #44b4b0 !important;
+  }
+</style>
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Crea Genere') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="max-w-md mx-auto bg-white rounded-lg overflow-hidden md:max-w-lg">
-                    <div class="md:flex">
-                        <div class="w-full px-4 py-6">
-                            <div class="md:flex">
-                                <div class="w-full px-4 py-6">
-                                    <form action="{{ route('genre.store') }}" method="POST">
-                                        @csrf
-                                        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                                            <div>
-                                                <label for="name" class="block text-sm font-medium text-gray-700">Nome</label>
-                                                <input type="text" name="name" id="name" placeholder="Nome" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                            </div>
-                                        </div>
-                                        <div class="mt-6">
-                                            <button type="submit" class="btn btn-outline-info">Crea</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Crea Genere') }}
+      </h2>
+  </x-slot>
 
 
 
-            </div>
-        </div>
-    </div>
+
+  <div class="py-12">
+       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+           <div class="glass-effect overflow-hidden shadow-sm sm:rounded-lg">
+               <div class="p-6 text-gray-900 text-center">
+               </div>
+               <div class="d-flex justify-content-center flex-column align-items-center text-center">
+                                  <form action="{{ route('genre.store') }}" method="POST" >
+                                     @csrf
+                                      <div >
+                                          <div>
+                                              <label for="name" class="block text-sm font-medium text-gray-700">Genere</label>
+                                              <input type="text" name="name" id="name" placeholder="Genere" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                          </div>
+                                      </div>
+                                      <div class="mt-6">
+                                          <button type="submit" class="btn btn-custom" style="width: 200px !important;">Crea</button>
+                                      </div>
+                                  </form>
+</div>
+                              </div>
+           </div>
+       </div>
+   </div>
 </x-app-layout>
