14de47a3e6b31d01f06d8c8932b01d4e82a20636
henristy <henristy2010@gmail.com>
2024-03-26 16:10:24 +0100

    hrey

--- librarium-app/resources/views/layouts/app.blade.php
+++ librarium-app/resources/views/layouts/app.blade.php
@@ -123,7 +123,7 @@
             </main>
         </div>
         @if(session()->has('message'))
-            <div id="action-message" class="fixed show text-black bottom-4 right-4 bg-green-500 px-4 py-2 rounded-md shadow-md">
+            <div id="action-message" class="fixed show text-black bottom-4 z-50 right-4 bg-green-500 px-4 py-2 rounded-md shadow-md">
                 {{ session('message') }}
             </div>
         @endif
